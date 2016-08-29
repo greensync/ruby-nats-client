61b14e515680aeb31392869262bbdca9afb10c03
Simon Russell <simon@greensync.com.au>
2016-08-29 14:00:54 +1000

    Pub/Sub commands.

--- lib/nats_client/sender.rb
+++ lib/nats_client/sender.rb
@@ -3,11 +3,18 @@
 
 class NatsClient::Sender
 
+  class InvalidTopicError < RuntimeError; end
+  class InvalidNameError < RuntimeError; end
+  class InvalidPayloadEncoding < RuntimeError; end
+
   def initialize(stream)
     @stream = stream
   end
 
   CONNECT_SPACE = "CONNECT ".freeze
+  PUB_SPACE = "PUB ".freeze
+  SUB_SPACE = "SUB ".freeze
+  SPACE = " ".freeze
   CR_LF = "\r\n".freeze
 
   DEFAULT_CONNECT_OPTIONS = {
@@ -18,10 +25,55 @@ class NatsClient::Sender
     protocol: NatsClient::PROTOCOL_VERSION
   }.freeze
 
+  VALID_NAME = /\A[a-z\d]+\z/i.freeze
+  VALID_TOPIC = /\A(?:(?:[a-z\d]+|\*)\.)*(?:[a-z\d]+|\*|\>)\z/i.freeze
+
   def connect!(info)
     @stream << CONNECT_SPACE << JSON.generate(DEFAULT_CONNECT_OPTIONS.merge(info)) << CR_LF
+
+    self
+  end
+
+  def pub!(topic, payload, options = {})
+    reply_topic = options.fetch(:reply_to, nil)
+
+    validate_topic!(topic)
+    validate_topic!(reply_topic) if reply_topic
+    validate_payload!(payload)
+
+    @stream << PUB_SPACE << topic << SPACE
+    @stream << reply_topic << SPACE if reply_topic
+    @stream << payload.bytesize << CR_LF << payload << CR_LF
+
+    self
+  end
+
+  def sub!(topic, subscription_id, options = {})
+    queue_group = options.fetch(:queue_group, nil)
+
+    validate_topic!(topic)
+    validate_name!(subscription_id)
+    validate_name!(queue_group) if queue_group
+
+    @stream << SUB_SPACE << topic << SPACE
+    @stream << queue_group << SPACE if queue_group
+    @stream << subscription_id << CR_LF
+
+    self
   end
 
   private
+
+  def validate_topic!(topic)
+    raise InvalidTopicError.new(topic) unless topic =~ VALID_TOPIC
+  end
+
+  def validate_name!(name)
+    raise InvalidNameError.new(name) unless name =~ VALID_NAME
+  end
+
+  def validate_payload!(payload)
+    raise InvalidPayloadEncoding.new(payload.encoding.name) unless payload.encoding == @stream.external_encoding
+  end
 
 end
--- spec/nats_client/sender_spec.rb
+++ spec/nats_client/sender_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe NatsClient::Sender do
 
-  let(:stream) { StringIO.new("".force_encoding('ASCII-8BIT')) }
+  let(:stream) { StringIO.new("") }
   let(:sender) { NatsClient::Sender.new(stream) }
 
   describe "#connect!" do
@@ -27,5 +27,136 @@ describe NatsClient::Sender do
     end
 
   end
+
+  describe "#pub!" do
+
+    let(:topic) { "FISHY.TIMES" }
+    let(:payload) { "MY FISHY MESSAGE" }
+    let(:options) { {} }
+
+    subject { sender.pub!(topic, payload, options); stream.string }
+
+    context "with no options" do
+
+      let(:options) { {} }
+
+      it "should format correctly" do
+        expect(subject).to eq("PUB #{topic} #{payload.bytesize}\r\n#{payload}\r\n")
+      end
+
+    end
+
+    context "with reply topic" do
+
+      let(:options) { { reply_to: "REPLY.BOX" } }
+
+      it "should format correctly" do
+        expect(subject).to eq("PUB #{topic} REPLY.BOX #{payload.bytesize}\r\n#{payload}\r\n")
+      end
+
+    end
+
+    context "with invalid topic" do
+
+      let(:topic) { "INVALID!TOPIC is fun" }
+
+      it "should format correctly" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidTopicError, topic)
+      end
+
+    end
+
+    context "with invalid reply topic" do
+
+      let(:options) { { reply_to: "INVALID!TOPIC is fun" } }
+
+      it "should format correctly" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidTopicError, "INVALID!TOPIC is fun")
+      end
+
+    end
+
+    context "with UTF-8 payload" do
+
+      let(:payload) { "\u{1f4a9}" }
+
+      it "should use the byte size" do
+        expect(subject).to eq("PUB #{topic} 4\r\n#{payload}\r\n")
+      end
+
+    end
+
+    context "with ASCII-8BIT payload" do
+
+      let(:payload) { "\u{1f4a9}".force_encoding('ascii-8bit') }
+
+      it "should use the byte size" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidPayloadEncoding, payload.encoding.name)
+      end
+
+    end
+
+  end
+
+  describe "sub!" do
+
+    let(:topic) { "BOBBY.TABLES" }
+    let(:subscription_id) { "XYZ123" }
+    let(:options) { {} }
+
+    subject { sender.sub!(topic, subscription_id, options); stream.string }
+
+    context "with no options" do
+
+      let(:options) { {} }
+
+      it "should format correctly" do
+        expect(subject).to eq("SUB #{topic} #{subscription_id}\r\n")
+      end
+
+    end
+
+    context "with queue group" do
+
+      let(:options) { { queue_group: "QU34" } }
+
+      it "should format correctly" do
+        expect(subject).to eq("SUB #{topic} QU34 #{subscription_id}\r\n")
+      end
+
+    end
+
+    context "with invalid topic" do
+
+      let(:topic) { "INVALID!TOPIC is fun" }
+
+      it "should format correctly" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidTopicError, topic)
+      end
+
+    end
+
+    context "with invalid subscription_id" do
+
+      let(:subscription_id) { "INVALID!TOPIC is fun" }
+
+      it "should format correctly" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidNameError, subscription_id)
+      end
+
+    end
+
+    context "with invalid queue group" do
+
+      let(:options) { { queue_group: "$$$" } }
+
+      it "should format correctly" do
+        expect { subject }.to raise_error(NatsClient::Sender::InvalidNameError, "$$$")
+      end
+
+    end
+
+  end
+
 
 end
